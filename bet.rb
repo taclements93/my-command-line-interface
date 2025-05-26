47d16648958845023c13b445b4df04454a513a17
Thomas Clements <163448648+taclements93@users.noreply.github.com>
2025-05-26 14:39:03 +0000

    added Class Bet

--- bet.rb
+++ bet.rb
@@ -7,48 +7,51 @@ their_answer = gets.chomp.downcase
 # puts "DEBUG: I read: ‹#{their_answer}›"
 
  
-
-
-def choice(answer)
-  if answer == "yes"
-    # puts "DEBUG: hit the YES branch"
-    load "./agree.rb"
-
-    puts "Even or Odd?"
-    guess = gets.chomp.downcase
-    
-    load "./roll_dice.rb"
-    outcome = roll_dice
-
-    
-
-    if outcome.even? && guess == "even" ||
-      outcome.odd? && guess == "odd"
-      puts "Aayyyy! You won!" 
-    else 
-      puts "Ope...that's rough buddy..."
-    end
-    puts "You rolled a #{outcome}"
-    puts "Roll again?"
-    retry_answer = gets.chomp.downcase
-    if retry_answer == "yes"
-      choice("yes")
-    else
-      puts "Then sashay away"
-    
-    end
-
-  elsif answer == "no"
-    # puts "DEBUG: hit the NO branch"
-    load "./decline.rb"
-  
-  else 
-    # puts "DEBUG: hit the ELSE branch"
-    puts "please answer yes or no"
+class Bet
+  def initialize
+    return
   end
+
+  def choice(answer)
+    if answer == "yes"
+    # puts "DEBUG: hit the YES branch"
+      load "./agree.rb"
+
+      puts "Even or Odd?"
+      guess = gets.chomp.downcase
+    
+      load "./roll_dice.rb"
+      outcome = roll_dice
+
+    
+
+      if outcome.even? && guess == "even" ||
+        outcome.odd? && guess == "odd"
+        puts "Aayyyy! You won!" 
+      else 
+        puts "Ope...that's rough buddy..."
+      end
+      puts "You rolled a #{outcome}"
+      puts "Roll again?"
+      retry_answer = gets.chomp.downcase
+      if retry_answer == "yes"
+        choice("yes")
+      else
+        puts "Then sashay away"
+    
+      end
+
+    elsif answer == "no"
+    # puts "DEBUG: hit the NO branch"
+      load "./decline.rb"
+  
+    else 
+    # puts "DEBUG: hit the ELSE branch"
+      puts "please answer yes or no"
+    end
+  end
+
 end
-
-
 
 choice(their_answer)
 
